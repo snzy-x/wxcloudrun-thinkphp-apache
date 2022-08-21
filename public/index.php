996c1a8a281b7696f45fa7f18fb35f78c489907e
snzy-x <99539117+snzy-x@users.noreply.github.com>
2022-08-22 00:22:21 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,7 +12,6 @@
 // [ 应用入口文件 ]
 namespace think;
 
-require __DIR__ . '/../vendor/autoload.php';
 
 // 执行HTTP应用并响应
 $http = (new App())->http;
@@ -22,3 +21,145 @@ $response = $http->run();
 $response->send();
 
 $http->end($response);
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- 防止右键-->
+		<meta itemprop="name" content="心安" />
+		<meta itemprop="image" content="img/logo.png" />
+		<meta name="description" itemprop="description" content="心安个人喜欢" />
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="stylesheet" type="text/css" href="css/neibu.css" />
+		<link rel="stylesheet" type="text/css" href="css/tc.css" />
+		<title>学习资源合集</title>
+		<style>
+			#3{
+				color: #FF0000;
+			}
+			#a1 {
+				font-size: 16px;
+			}
+
+			#a {
+				font-size: 20px;
+				color: #FF0000;
+			}
+
+			#b {
+				font-size: 30px;
+			}
+
+			#c {
+				font-size: 20px;
+			}
+
+			#gg {
+				font-size: 15px;
+				color: #ff0000;
+				font-family: 楷体;
+				margin-top: -1px;
+				/* 公告 */
+			}
+			#x{font-size: 20px;
+			color: #ff0000;
+			font-family: 楷体;}
+
+			#marquee {
+				font-size: 15px;
+				margin-left: 33px;
+				color: #ff0000;
+				font-family: 楷体;
+				margin-top: -1px;
+				/* 滚动 */
+			}
+			#sj{
+				font-size: 35px;
+				color: #FF0000;
+			}
+		</style>
+	</head>
+	<center>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script src='js/sj.js' type='text/javascript'></script>
+		<span id="Clock"></span>
+		<span id="Cloc"></span></div>
+	</center>
+	<body>
+		<div id="box">
+			<div id="top">
+				<center>
+					<img id="logo" src="img/logo.png" width="130px" height="130px">
+				</center>
+			</div>
+			<body onload="welcome();">
+				<div id="nav">
+					<ul class="navbar">
+						<ul>
+							<li><a href="https://www.itsk.com" target="imp" id="a1">IT天空</a></li>
+							<li><a href="https://msdn.itellyou.cn" target="imp" id="a1">msdn</a></li>
+							<li><a href="https://www.mycodes.net" target="imp" id="a1">源码之家</a></li>
+							<li><a href="http://www.wepe.com.cn" target="imp" id="a1">微PE</a></li>
+							<li><a href="https://huaban.com" target="imp" id="a1">花瓣网</a></li>
+							<li><a href="https://www.runoob.com" target="imp" id="a1">菜鸟教程</a></li>
+							<li><a href="https://www.softwhy.com" target="imp" id="a1">蚂蚁部落</a></li>
+							<li><a href="http://www.mydiskless.com" target="imp" id="a1">无盘之家</a></li>
+							<li><a href="https://www.w3school.com.cn/index.html" target="imp" id="a1">编程学习</a></li>
+							<li><a href="https://www.jb51.net/" target="imp" id="a1">脚本之家</a></li>
+							<li><a href="https://yuanmajie.com" target="imp" id="a1">源码街</a></li>
+						</ul>
+					</ul>
+					<div id="led">
+						<div style="position:absolute;" id="gg">公告:</div>
+						<div id="z"><a href="http://wpa.qq.com/msgrd?v=3&uin=2981874265&site=qq&menu=yes">
+								<!-- 添加QQ -->
+								<img border="0" src="img/QQ.jpg" width="100px" height="100px" /></a><br><!-- 添加QQ -->
+							<a href="https://jq.qq.com/?_wv=1027&k=S1noulJr" target="imp">
+								<!-- 添加QQ群 -->
+								<img border="0" src="img/qun.jpg" width="100px" height="100px" /></a><!-- 添加QQ群 -->
+						</div>
+						<marquee behavior="alternate" scrollamount="10" id="marquee">欢迎你的到来！</a><br></marquee>
+					</div>
+					<iframe src="https://www.itsk.com" id="imp" name="imp" width="100%" height="3900px" scrolling="yes" frameborder="0"
+					 top="0px"></iframe>
+					<!--打开网页显示的-->
+					<div id="footer">
+						<div id="a">
+							<center>☆心安☆</center>
+						</div>
+						<center>
+							<font size="" color="#ff0000" id="b">多学习吧！少年！<br>吾日三省自身</font><br>
+							<a href="http://wpa.qq.com/msgrd?v=3&uin=2981874265&site=qq&menu=yes">
+								<!-- 添加QQ -->
+								<img border="0" src="img/QQ.jpg" width="100px" height="100px" /></a><!-- 添加QQ -->
+							<a href="https://jq.qq.com/?_wv=1027&k=S1noulJr" target="imp">
+								<!-- 添加QQ群 -->
+								<img border="0" src="img/qun.jpg" width="100px" height="100px" /></a><!-- 添加QQ群 -->
+							<marquee behavior="alternate" scrollamount="5" id="sj">
+								<p id="main">
+									不知不觉我们已经分开了<span></span>
+								</p>
+								<script src='js/js.js' type='text/javascript'></script>
+							</marquee>
+						</center>
+					</div>
+				</div>
+				<div id="abc">
+					<script src='js/tc.js' type='text/javascript'></script>
+					<script src='js/gb.js' type='text/javascript'></script>
+					<div class="swal-overlay swal-overlay--show-modal" tabindex="-1">
+						<div class="swal-modal" role="dialog" aria-modal="true">
+							<div class="swal-icon swal-icon--success">
+								<span class="swal-icon--success__line swal-icon--success__line--long"></span>
+								<span class="swal-icon--success__line swal-icon--success__line--tip"></span>
+								<div class="swal-icon--success__ring"></div>
+								<div class="swal-icon--success__hide-corners"></div>
+							</div>
+							<div class="swal-title" style="">心安</div>
+							<div class="swal-text" style="">欢迎你的到来!<br><br><a href="http://wpa.qq.com/msgrd?v=3&uin=2981874265&site=qq&menu=yes">联系QQ:2981874265</a><br><br><a href="https://jq.qq.com/?_wv=1027&k=S1noulJr" target="imp">联系QQ群:700232482<br><br></a></div>
+							<div class="swal-footer">
+								<div class="swal-button-container">
+									<button class="swal-button swal-button--confirm" onclick="javascript:turnoff('abc')">关闭</button>
+									</a>
+			</body>
+</html>
